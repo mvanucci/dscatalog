38ed4adbfe1f1c72077a33ad70588bc83481b17a
Murilo Vanucci <murilo_vanucci@hotmail.com>
2024-01-14 13:25:41 -0300

    feat: added new method createCategory

--- src/test/java/com/devsuperior/dscatalog/tests/Factory.java
+++ src/test/java/com/devsuperior/dscatalog/tests/Factory.java
@@ -9,12 +9,16 @@ import com.devsuperior.dscatalog.entities.Product;
 public class Factory {
 	public static Product createProduct() {
 		Product product =  new Product(1L, "Phone", "Good Phone", 800.0, "https://img.com/img.png", Instant.parse("2024-10-20T03:00:00Z"));
-		product.getCategories().add(new Category(2L, "Eletronincs"));
+		product.getCategories().add(new Category(1L, "Electronics"));
 		return product;
 	}
 	
 	public static ProductDTO createProductDTO() {
 		Product product = createProduct();
 		return new ProductDTO(createProduct(), product.getCategories());
+	}
+	
+	public static Category createCategory() {
+		return new Category(1L, "Electronics");
 	}
 }
