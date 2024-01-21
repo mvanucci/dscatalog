2f0287746e123e08777af03273c4725918ccd15d
Murilo Vanucci <murilo_vanucci@hotmail.com>
2024-01-21 11:13:03 -0300

    feat: beans validation without custom validation

--- src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -6,6 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
 
@@ -13,10 +18,16 @@ public class ProductDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+	
+	@NotBlank(message="Campo requerido")
+	@Size(min = 5, max = 60, message="O nome deve ter entre 5 a 60 caracteres")
 	private String name;
 	private String description;
+	@Positive(message= "O preço deve ser positivo")
 	private Double price;
 	private String imgUrl;
+	
+	@PastOrPresent(message = "A data do produto não pode ser futura")
 	private Instant date;
 	
 	private List<CategoryDTO> categories = new ArrayList<>();
